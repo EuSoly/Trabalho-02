6d1105407d44c1d8dcba2a82831ebd4becfbb5f2
EuSoly <130078074+EuSoly@users.noreply.github.com>
2024-05-07 10:48:26 -0300

    Adicionado o código

--- potencia.py
+++ potencia.py
@@ -1,1 +1,8 @@
+base = float(input("Digite o valor da base: "))
+expo = int(input("Digite o valor do expoente: "))
 
+resultado = ()
+
+for tabela in range (1 , expo):
+    resultado = base * base
+print("o resultado de", base , "elevado à", expo,"é", resultado)
