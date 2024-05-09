c257a86e2c56478af3454a9cf8b6a8d611bde558
EuSoly <130078074+EuSoly@users.noreply.github.com>
2024-05-09 11:32:37 -0300

    Update Taxa de crescimente.py

--- Taxa de crescimente.py
+++ Taxa de crescimente.py
@@ -1,1 +1,13 @@
+A = 200
+B = 2000
+A1 = 1.5
+B1 = 3.0
+Av = int()
 
+while A < B:
+    A = A * A1
+    Av = Av + 1
+while A > B:
+    print("Com", A, "Habitantes")
+    print("Precisou de", Av, "Anos para chegar á população de B" )
+    break
