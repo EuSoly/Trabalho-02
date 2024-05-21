a2b3f0061af54f17b42a86a94eb350e4d9faf422
EuSoly <130078074+EuSoly@users.noreply.github.com>
2024-05-21 08:57:04 -0300

    Update Fibonacci.py

--- Fibonacci.py
+++ Fibonacci.py
@@ -1,1 +1,11 @@
+quanto = int(input("quantas casa você quer? "))
+fim = 0
+numA = 1
+numB = 0
 
+while fim < quanto:
+    num = numA + numB
+    numB = numA 
+    numA = num
+    print (num)
+    fim = fim + 1
