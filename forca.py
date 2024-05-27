27f45a921ae78db0fdc6ea737ef77c86bdec287b
EuSoly <130078074+EuSoly@users.noreply.github.com>
2024-05-27 11:28:31 -0300

    Update forca.py

--- forca.py
+++ forca.py
@@ -1,8 +1,14 @@
+import random
 
-palavra = "atum"
-guest = ""
+def Sorteio(palavras):
+    palavras = ["none", "arroz", "atum", "bolacha", "cozinha", "laranja", "espinafre", "matematica", "morango", "teclado", "paralelepipedo" ]
+    sorter = random.randint(1,10)
+    palavra = (palavras[sorter])
+    print(palavra)
+    
+
+
 def forca(tentativa):
-
     f1 = "    +--------+  "
     f2 = "    |           "
     f3 = "    |           "
@@ -11,9 +17,8 @@ def forca(tentativa):
     f6 = "    |           "
     f7 = "    |           "
     f8 = "____|____       "
-    guest = input("fgfgfgf")
     
-    
+
     if tentativa >= 1:
         f2 = f2 = "    |        |  "
         f3 = f3 = "    |        0  "
@@ -38,12 +43,16 @@ def forca(tentativa):
         
     if tentativa >= 8:
         print("Você perdeu!!")
-    
+        print(f5)
 
 def tent(tent):
     sim = input("Deseja jogar? S/N:\n")
     if sim.lower() == "s":
-        return forca
+        forca(0)
     else:
         print("Então vá pa puta que pariu")
+
+
 tent(0)
+Sorteio(0)
+
