d24afac1c4025dc8d1c1fc231760861feeaba413
EuSoly <130078074+EuSoly@users.noreply.github.com>
2024-06-04 10:56:42 -0300

    Update forca.py

--- forca.py
+++ forca.py
@@ -1,14 +1,11 @@
 import random
-
-def Sorteio(palavras):
+erros = 0
+def Sorteio(palavra):
     palavras = ["none", "arroz", "atum", "bolacha", "cozinha", "laranja", "espinafre", "matematica", "morango", "teclado", "paralelepipedo" ]
     sorter = random.randint(1,10)
     palavra = (palavras[sorter])
-    print(palavra)
-    
-
-
-def forca(tentativa):
+    return palavra
+def Forca(tentativa):
     f1 = "    +--------+  "
     f2 = "    |           "
     f3 = "    |           "
@@ -17,7 +14,6 @@ def forca(tentativa):
     f6 = "    |           "
     f7 = "    |           "
     f8 = "____|____       "
-    
 
     if tentativa >= 1:
         f2 = f2 = "    |        |  "
@@ -43,16 +39,30 @@ def forca(tentativa):
         
     if tentativa >= 8:
         print("Você perdeu!!")
-        print(f5)
-
-def tent(tent):
+    
+    print(f1)
+    print(f2)
+    print(f3)
+    print(f4)
+    print(f5)
+    print(f6)
+    print(f7)
+    print(f8)
+    return tentativa
+def Tent():
     sim = input("Deseja jogar? S/N:\n")
     if sim.lower() == "s":
-        forca(0)
+        Forca(0)
+        
     else:
         print("Então vá pa puta que pariu")
 
-
-tent(0)
-Sorteio(0)
+Tent()
+while 0 < 9:
+    tentativa = input("Digite a palavra")
+    if tentativa == Sorteio:
+        print("Acertou")
+    if tentativa != Sorteio:
+        erros = erros + 1
+        Forca(erros)
 
